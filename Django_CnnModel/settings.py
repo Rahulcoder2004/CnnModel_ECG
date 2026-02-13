687a3e512d88f56d7b549b61878f2d0a46d56189
Rahul A D <adrahul04@gmail.com>
2026-02-13 19:55:18 +0530

    Add python-dotenv dependency

--- Django_CnnModel/settings.py
+++ Django_CnnModel/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s#eo-5ivni1c%t^99*xut*je@)%5r8#6@t(yvv4=8a)!n)td(6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
